d10e5e5ea0f422aab6eeeead41579090ebc9a200
the-akira <gabrielfelippe90@gmail.com>
2019-11-14 00:18:27 -0300

    initial commit

---
+++ mysite/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from app import views
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('test/', views.test, name='test'),
+  path('profile/', views.profile, name='profile'),
+]
